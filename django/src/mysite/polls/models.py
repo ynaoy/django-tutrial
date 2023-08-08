422f21158d5c88cd0c508f2c0f2af0f8696349f4
ynaoy <naozou22113@yahoo.co.jp>
2023-08-08 15:07:27 +0900

    tutrialの第7章の内容を追加

--- django/src/mysite/polls/models.py
+++ django/src/mysite/polls/models.py
@@ -1,13 +1,20 @@
+from django.contrib import admin
 import datetime
-
 from django.db import models
 from django.utils import timezone
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean = True,
+        ordering = "pub_date",
+        description = "Published recently?",
+    )
 
     def was_published_recently(self):
         now = timezone.now()
